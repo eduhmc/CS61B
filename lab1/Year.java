9d1b58b1764f7cc24033789af54ca443627c9d81
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-08-30 16:09:23 -0700

    lab1

--- lab1/Year.java
+++ lab1/Year.java
@@ -5,10 +5,12 @@ public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        if ((year % 400 == 0) && (year % 100 != 0) || (year & 4 == 0))
-        return true;    // TODO: YOUR CODE HERE
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
     /** Print whether YEAR is a a leap year on System.out. */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {

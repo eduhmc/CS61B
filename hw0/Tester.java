ad2ff0e9146f0a08ed762addbb438f89c5ed4e37
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-09-02 17:42:58 -0700

    federer

--- hw0/Tester.java
+++ hw0/Tester.java
@@ -24,21 +24,21 @@ public class Tester {
     @Test
     public void maxTest() {
         // Change call to max to make this call yours.
-        assertEquals(14, hw0.max(new int[] { 0, -5, 2, 14, 10 }));
+        assertEquals(14, max.max(new int[] { 0, -5, 2, 14, 10 }));
         // REPLACE THIS WITH MORE TESTS.
     }
 
     @Test
     public void threeSumTest() {
         // Change call to threeSum to make this call yours.
-        assertTrue(hw0.threeSum(new int[] { -6, 3, 10, 200 }));
+        assertTrue(threeSum.threeSum(new int[] { -6, 3, 10, 200 }));
         // REPLACE THIS WITH MORE TESTS.
     }
 
     @Test
     public void threeSumDistinctTest() {
         // Change call to threeSumDistinct to make this call yours.
-        assertFalse(hw0.threeSumDistinct(new int[] { -6, 3, 10, 200 }));
+        assertFalse(threeSumDistinct.threeSumDistinct(new int[] { -6, 3, 10, 200 }));
         // REPLACE THIS WITH MORE TESTS.
     }
 

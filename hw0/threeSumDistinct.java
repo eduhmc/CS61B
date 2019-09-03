8f66822f281f8528523e24c6cd14894a28cdc1a7
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-09-02 17:37:32 -0700

    orian

---
+++ hw0/threeSumDistinct.java
@@ -0,0 +1,23 @@
+class threeSumDistinct{
+    public static void main(String[] input) {
+      int[] a=new int[input.length];
+       for(int i=0; i<input.length; i++){
+          a[i]=Integer.parseInt(input[i]);
+       }
+       System.out.println(threeSumDistinct(a));
+    }
+    public static boolean threeSumDistinct(int[] a){
+
+     for(int i=0; i<a.length; i++){
+        int x=a[i];
+        for(int j=i+1; j<a.length; j++){
+          int y=a[j];
+          for(int k=j+1; k<a.length; k++){
+              int z=a[k];
+              if(x+y+z==0){return true;}
+           }
+        }
+      }
+      return false;
+      }
+}

85712ec8e698d1c3471f4b1da7de1c66148814ee
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-09-03 11:56:13 -0700

    addign files back in

---
+++ hw0/max.java
@@ -0,0 +1,16 @@
+class max{
+	public static int max(int[] a){
+		int temp = a[0];
+		for (int i = 0; i < a.length; i++){
+			if (a[i]> temp){
+				temp = a[i];
+			}
+		}
+		return temp;
+
+	}
+	public static void main(String[] args){
+		int[] numbers = new int[]{9, 42, 15, 2, 22, 10, 6};
+		System.out.println("The largest number of the array is: " + max(numbers));
+	}
+}

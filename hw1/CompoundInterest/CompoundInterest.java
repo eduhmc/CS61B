1a2a60d8e95cabb3e0f5110b78ab07441840d389
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-09-08 21:58:29 -0700

    adding

--- hw1/CompoundInterest/CompoundInterest.java
+++ hw1/CompoundInterest/CompoundInterest.java
@@ -10,7 +10,7 @@ public class CompoundInterest {
      *  should be 1. Throughout the assignment it is OK to assume that
      *  TARGETYEAR is >= THIS_YEAR. */
     static int numYears(int targetYear) {
-        return 0;
+        return targetYear - THIS_YEAR;
     }
 
     /** Suppose we have an asset worth PRESENTVALUE that appreciates
@@ -22,7 +22,7 @@ public class CompoundInterest {
      *  then the futureValue will be 10*1.12*1.12 = 12.544. */
     static double futureValue(double presentValue, double rate,
                               int targetYear) {
-        return 0;
+        return presentValue * Math.pow((1+rate*0.01),targetYear-THIS_YEAR);
     }
 
     /** Returns returns the value, in THIS_YEAR dollars, of an asset
@@ -36,7 +36,7 @@ public class CompoundInterest {
      *  2019 dollars, we get 12.544 * 0.97 * 0.97 = 11.8026496 dollars. */
     static double futureValueReal(double presentValue, double rate,
                                   int targetYear, double inflationRate) {
-        return 0;
+        return presentValue*Math.pow((1+rate*0.01),targetYear-THIS_YEAR) * Math.pow((1-inflationRate*0.01),targetYear-THIS_YEAR);
     }
 
     /** Suppose you invest PERYEAR dollars at the end of every year until
@@ -47,7 +47,13 @@ public class CompoundInterest {
      *  then the result will be 5000*1.1*1.1 + 5000*1.1 + 5000 =
      *  16550. */
     static double totalSavings(double perYear, int targetYear, double rate) {
-        return 0;
+        result = perYear*Math.pow((1+rate*0.01),targetYear-THIS_YEAR);
+        // System.out.println(targetYear);
+        if(targetYear>THIS_YEAR){
+            result += totalSavings(perYear,targetYear-1,rate);
+            targetYear -=1;
+        }
+        return result;
     }
 
     /** Returns totalSavings(PERYEAR, TARGETYEAR, RATE) converted to
@@ -55,7 +61,15 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static double totalSavingsReal(double perYear, int targetYear, double rate,
                                double inflationRate) {
-        return 0;
+        double result;
+        result = perYear*Math.pow((1+rate*0.01),targetYear-THIS_YEAR)*
+                Math.pow((1-inflationRate*0.01),targetYear-THIS_YEAR);
+        // System.out.println(targetYear);
+        if(targetYear>THIS_YEAR){
+            result += totalSavingsReal(perYear,targetYear-1,rate,inflationRate);
+            targetYear -=1;
+        }
+        return result;
 
     }
 
@@ -65,8 +79,8 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static void printDollarFV(int targetYear, double returnRate,
                               double inflationRate) {
-        double nominalDollarValue = 0; // replace 0 with your code
-        double realDollarValue = 0;    // replace 0 with your code
+        double nominalDollarValue = futureValue(1,returnRate,targetYear); // replace 0 with your code; // replace 0 with your code
+        double realDollarValue = futureValueReal(1,returnRate,targetYear,inflationRate);;    // replace 0 with your code
 
         // Do not change anything in this method below this line
         String dollarSummary =
--- hw1/CompoundInterest/CompoundInterestTest.java
+++ hw1/CompoundInterest/CompoundInterestTest.java
@@ -8,27 +8,36 @@ public class CompoundInterestTest {
         /** Sample assert statement for comparing integers.
 
         assertEquals(0, 0); */
+        assertEquals(2, CompoundInterest.numYears(2021));
+
     }
 
     @Test
     public void testFutureValue() {
+
         double tolerance = 0.01;
+        assertEquals(12.544, CompoundInterest.futureValue(10.0,12,2021), tolerance);
     }
 
     @Test
     public void testFutureValueReal() {
+
         double tolerance = 0.01;
+        assertEquals(11.802, CompoundInterest.futureValueReal(10.0,12,2021,3), tolerance);
     }
 
 
     @Test
     public void testTotalSavings() {
+
         double tolerance = 0.01;
+        assertEquals(16550, CompoundInterest.totalSavings(5000,2021,10), tolerance);
     }
 
     @Test
     public void testTotalSavingsReal() {
         double tolerance = 0.01;
+        assertEquals(16027.445, CompoundInterest.totalSavingsReal(5000,2021,10,3), tolerance);
     }
 
 

d799761fad4fd376fb23c20fa9a790ee5c3109f6
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-10-01 02:34:17 -0700

    per

--- proj1/enigma/Permutation.java
+++ proj1/enigma/Permutation.java
@@ -49,47 +49,72 @@ class Permutation {
     /** Return the result of applying this permutation to P modulo the
      *  alphabet size. */
     int permute(int p) {
-        char pstring = _alphabet.toChar(p);
+        int preal = wrap(p); char pstring = _alphabet.toChar(preal);
+        String str = ""; int index = 0;
         for (int i = 0; i < _ciclos.size(); i++){
-            if (_ciclos.contains(p)){
-
+            if (_ciclos.get(i).indexOf(pstring) != -1){
+                index = _ciclos.get(i).indexOf(pstring);
+                str = _ciclos.get(i);
+                return _alphabet.toInt(str.charAt((index + 1) % str.length()));
             }
-
         }
+        return _alphabet.toInt(pstring);
 
 
-
-
-        return 0;  // FIXME
+        //return 0;  // FIXME
     }
 
     /** Return the result of applying the inverse of this permutation
      *  to  C modulo the alphabet size. */
     int invert(int c) {
-        return 0;  // FIXME
+        int creal = wrap(c); char cstring = _alphabet.toChar(creal);
+        String str2 = ""; int index2 = 0;
+        for (int i = 0; i < _ciclos.size(); i++){
+            if (_ciclos.get(i).indexOf(cstring) != -1){
+                index2 = _ciclos.get(i).indexOf(cstring);
+                str2 = _ciclos.get(i);
+                return _alphabet.toInt(str2.charAt((index2 - 1) % str2.length()));
+            }
+        }
+        return _alphabet.toInt(cstring);
+
+        //return 0;  // FIXME
     }
 
     /** Return the result of applying this permutation to the index of P
      *  in ALPHABET, and converting the result to a character of ALPHABET. */
     char permute(char p) {
+        int inchar = _alphabet.toInt(p);
+        int sol = permute(inchar);
+        return _alphabet.toChar(sol);
 
-        return 0;  // FIXME
+        // return 0;  // FIXME
     }
 
     /** Return the result of applying the inverse of this permutation to C. */
     int invert(char c) {
-        return 0;  // FIXME
+        int inchar2 = _alphabet.toInt(c);
+        int sol2 = invert(inchar2);
+        return _alphabet.toChar(sol2);
+
+        //return 0;  // FIXME
     }
 
     /** Return the alphabet used to initialize this Permutation. */
     Alphabet alphabet() {
+
         return _alphabet;
     }
 
     /** Return true iff this permutation is a derangement (i.e., a
      *  permutation for which no value maps to itself). */
     boolean derangement() {
-        return true;  // FIXME
+        int contador = 0;
+        for (int i = 0; i < _ciclos.size(); i += 1) {
+            contador = contador + _ciclos.get(i).length();
+        }
+        return contador == _alphabet.size();
+        //return true;  // FIXME
     }
 
     /** Alphabet of this permutation. */

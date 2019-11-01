0b58aaea63e116511398097c93e1d7399f5dcc83
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-11-01 00:54:42 -0400

    hola

--- hw6/BSTStringSet.java
+++ hw6/BSTStringSet.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Stack;
+import java.util.*;
 
 /**
  * Implementation of a BST based String Set.
@@ -19,54 +20,22 @@ public class BSTStringSet implements StringSet, Iterable<String> {
         // FIXME: PART A
         _root = put(s, _root);
     }
-    private Node put(String s, Node nodal){
-        if (nodal == null){
-            return new Node(s);
-        }
-        int prueba = nodal.s.compareTo(s);
-        if (prueba > 0) {
-            nodal.left = put(s, nodal.left);
-        } else if (prueba < 0){
-            nodal.right = put(s, nodal.right);
-        }
-        return nodal;
-    }
+
 
     @Override
     public boolean contains(String s) {
-        //return false; // FIXME: PART A
+        // FIXME: PART A
         return contains(s, _root);
 
     }
-    private boolean contains(String s, Node nodal){
-        if (nodal == null){
-            return false;
-        }
-        if (nodal.s.equals(s)){
-            return true;
-        } else {
-            if (nodal.s.compareTo(s) > 0){
-                return contains(s, nodal.left);
-            } else{
-                return contains(s, nodal.right);
-            }
-        }
-    }
+
 
     @Override
     public List<String> asList() {
-        //return null; // FIXME: PART A
+        // FIXME: PART A
         List<String> result = new ArrayList<>();
         asList(result, _root);
         return result;
-    }
-    private  void asList(List<String> lst, Node nodal){
-        if (nodal == null){
-            return;
-        }
-        asList(lst, nodal.left);
-        lst.add(nodal.s);
-        asList(lst, nodal.right);
     }
 
     /** Represents a single Node of the tree. */
@@ -131,21 +100,64 @@ public class BSTStringSet implements StringSet, Iterable<String> {
 
     @Override
     public Iterator<String> iterator() {
+
         return new BSTIterator(_root);
+    }
+
+    private Node put(String s, Node federer){
+        if (federer == null){
+            return new Node(s);
+        }
+        int prueba = federer.s.compareTo(s);
+        if (prueba > 0) {
+            federer.left = put(s, federer.left);
+        } else if (prueba < 0){
+            federer.right = put(s, federer.right);
+        }
+        return federer;
+    }
+    private boolean contains(String s, Node federer){
+        if (federer == null){
+            return false;
+        }
+        if (federer.s.equals(s)){
+            return true;
+        } else {
+            if (federer.s.compareTo(s) > 0){
+                return contains(s, federer.left);
+            } else{
+                return contains(s, federer.right);
+            }
+        }
+    }
+
+    private  void asList(List<String> lst, Node newnode){
+        if (newnode == null){
+            return;
+        }
+        asList(lst, newnode.left);
+        lst.add(newnode.s);
+        asList(lst, newnode.right);
     }
 
     // FIXME: UNCOMMENT THE NEXT LINE FOR PART B
     //@Override
     public Iterator<String> iterator(String low, String high) {
-        List<String> lst = asList();
-        List<String> retlist = new ArrayList<String>();
-        for (String i: lst){
-            if (i.compareTo(low) < 0 && i.compareTo(high) > 0){
-                retlist.add(i);
+        List<String> nuevo = asList();
+        int bajo = 0;
+        int alto = nuevo.size();
+        for (int i = 0; i < nuevo.size(); i++){
+            int check = low.compareTo(nuevo.get(i));
+            if (check > 0){
+                bajo = i + 1;
+            }
+            int chequeo = high.compareTo(nuevo.get(i));
+            if (chequeo < 0){
+                alto = i;
+                break;
             }
         }
-        return retlist.iterator();
-
+        return nuevo.subList(bajo, alto).iterator();
     }
 
     /** Root node of the tree. */

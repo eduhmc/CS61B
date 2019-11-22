a606e05d437ca9c6e8ab4df868f40818f3685b2a
Eduardo Huerta Mercado <eduhmc@berkeley.edu>
2019-11-22 01:00:06 -0800

    JJJ

--- lab13/MazeBreadthFirstPaths.java
+++ lab13/MazeBreadthFirstPaths.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -7,6 +12,11 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     protected int[] edgeTo;
     protected boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+
 
     /** A breadth-first search of paths in M from (SOURCEX, SOURCEY) to
      *  (TARGETX, TARGETY). */
@@ -14,18 +24,45 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
                                  int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo,
         // and marked, as well as call announce()
+        marked[v] = true;
+        announce();
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+
+        while (!q.isEmpty()) {
+            int i = q.remove();
+            for (int w: maze.adj(i)) {
+                if (i == t) { targetFound = true; }
+                if (targetFound) { return; }
+                if (!marked[w]) {
+                    edgeTo[w] = i;
+                    announce();
+                    distTo[w] = distTo[i] + 1;
+                    announce();
+                    marked[w] = true;
+                    announce();
+                    q.add(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab13/MazeCycles.java
+++ lab13/MazeCycles.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Observable;
 /**
  *  @author Josh Hug
  */
@@ -8,7 +11,9 @@ public class MazeCycles extends MazeExplorer {
     protected int[] edgeTo;
     protected boolean[] marked;
     */
-
+    private boolean targetFound = false;
+    private Maze maze;
+    private int[] temp;
     /** Set up to find cycles of M. */
     public MazeCycles(Maze m) {
         super(m);
@@ -17,8 +22,39 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        solve_helper(temp, 0);
     }
 
     // Helper methods go here
+    public void solve_helper(int[] i, int s) {
+        if (marked[s]) {
+            targetFound = true;
+            edgeTo[s] = i[s];
+            announce();
+            int next = i[s];
+            while (next != s) {
+                edgeTo[next] = i[next];
+                announce();
+                next = i[next];
+            }
+        }
+        if (targetFound) {
+            return;
+        }
+
+        marked[s] = true;
+        announce();
+
+        for (int w : maze.adj(s)) {
+            if (w != i[s]) {
+                i[w] = s;
+                solve_helper(i, w);
+                if (targetFound) {
+                    return;
+                }
+            }
+        }
+
+    }
 }
 
